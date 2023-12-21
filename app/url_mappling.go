8a9bf2f1eb901d0a086b9914336f58f4c9918085
sakibcoolz <sakibcoolz@gmail.com>
2023-12-21 19:49:54 +0530

    Added correction

--- app/url_mappling.go
+++ app/url_mappling.go
@@ -52,7 +52,7 @@ func Apps(config *config.Config, logger *zap.Logger, router *gin.Engine) *gin.En
 
 	router.GET("/:path", controller.GetUrl)
 
-	preapproute := router.Group("/" + os.Getenv(literals.VERSION))
+	preapproute := router.Group("/v1")
 
 	preapproute.GET(literals.HEALTH, controller.Health)
 
--- controller/health.go
+++ controller/health.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"net/http"
+	"os"
+	"zlink/literals"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,5 +11,5 @@ import (
 func (c *Controller) Health(ctx *gin.Context) {
 	c.log.Info("health status API")
 
-	ctx.JSON(http.StatusOK, gin.H{"status": "ok"})
+	ctx.JSON(http.StatusOK, gin.H{"status": os.Getenv(literals.VERSION)})
 }
